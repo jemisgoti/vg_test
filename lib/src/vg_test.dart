97fb249bbc06d8eb0824362c5b132bbad9c8bb9b
Jemis Goti <46031164+jemisgoti@users.noreply.github.com>
2024-09-15 20:57:17 +0530

    initial commit

---
+++ lib/src/vg_test.dart
@@ -0,0 +1,7 @@
+/// {@template vg_test}
+/// test project
+/// {@endtemplate}
+class VgTest {
+  /// {@macro vg_test}
+  const VgTest();
+}
---
+++ lib/vg_test.dart
@@ -0,0 +1,4 @@
+/// test project
+library vg_test;
+
+export 'src/vg_test.dart';
