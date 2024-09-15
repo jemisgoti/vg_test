b0bab4884ccde5859609407751f58da16b98ab0d
jemisgoti <jemisgoti@users.noreply.github.com>
2024-09-15 16:02:10 +0000

    deploy: 3f82622e34163a03e760d0b276e36de7df36019c

--- lib/src/vg_test.dart
+++
@@ -1,7 +0,0 @@
-/// {@template vg_test}
-/// test project
-/// {@endtemplate}
-class VgTest {
-  /// {@macro vg_test}
-  const VgTest();
-}
--- lib/vg_test.dart
+++
@@ -1,4 +0,0 @@
-/// test project
-library vg_test;
-
-export 'src/vg_test.dart';
